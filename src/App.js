6c5d9081cf965d44d32b50423633ad485ca6e165
Niall Lennon <niall.lennon2@gmail.com>
2018-03-23 15:13:54 -0700

    create grid layout

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header>Header</header>
+        <div class="ad1" id="ad1">Ad 1</div>
+        <div class="message" id="message">Message</div>
+        <div class="button" id="button">Button</div>
+        <div class="checkboxes" id="checkboxes">Checkbox List</div>
+        <div class="ad2" id="ad2">Ad 2</div>
+        <footer>Footer</footer>
       </div>
     );
   }
