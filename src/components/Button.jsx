13b19bb39838ae2f94ba44637ac0ca12bfaa9d25
Niall Lennon <niall.lennon@unbounce.com>
2018-06-01 16:09:28 -0700

    add icon to the button; style button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import Logo from '../components/icon-svg';
 import { updateMessage } from '../actions';
 
+import './Button.css';
+
 const Button = ({ updateMessage, categories }) => {
-    return <button onClick={updateMessage}>Click Me</button>;
+  return (
+    <div className='buttonContainer'>
+      <button className='button' onClick={updateMessage}>Generate
+        <Logo />
+      </button>
+    </div>
+  );
 }
 
 export default connect(null, {updateMessage})(Button);
